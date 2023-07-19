e299bbef73bbc7b2f4f1e4646f391f948b39862d
Sava2901 <savasergiu2006@gmail.com>
2023-07-19 19:13:42 +0300

    finished the project

--- index-script.js
+++ index-script.js
@@ -1,77 +1,117 @@
-const library  = document.querySelector(".library");
+const library = document.querySelector(".library");
 const popUp = document.querySelector(".modal");
 const newBook = document.querySelector(".newBookButton");
 const submitButton = document.querySelector(".submitButton");
 const closeButton = document.querySelector(".closePopUpButton");
 const removeButton = document.querySelectorAll(".removeButton");
-const title = document.querySelector("#title");
-const author = document.querySelector("#author");
-const pages = document.querySelector("#pages");
+const allBooks = document.querySelector(".allBooks");
+const readBooks = document.querySelector(".readBooks");
+const totalPages = document.querySelector(".totalPages");
+const read = document.querySelector(".read");
+let bool = false;
+let title = document.querySelector("#title");
+let author = document.querySelector("#author");
+let pages = document.querySelector("#pages");
 
-newBook.addEventListener("click", () => {
+
+newBook.addEventListener("click", (e) => {
     popUp.style.display = "flex";
+    e.preventDefault();
 });
 
-submitButton.addEventListener("click", () => {
+closeButton.addEventListener("click", () => {
     popUp.style.display = "none";
 });
 
-removeButton.forEach(button => {
-    button.addEventListener("click", () => {
-        button.parentNode.remove();
-    });
+read.addEventListener("click", () => {
+    bool = !bool;
 });
 
-submitButton.addEventListener("click", () => {
-    const book = document.createElement("div");
-    book.classList.add("book");
-    
-    const Title = document.createElement("p");
-    Title.classList.add("bookTitle");
-    Title.classList.add("text");
-    Title.textContent = title.value;
-    book.appendChild(Title);
+submitButton.addEventListener("click", (e) => {
 
-    const Author = document.createElement("p");
-    Author.classList.add("bookAuthor");
-    Author.classList.add("text");
-    Author.textContent = author.value;
-    book.appendChild(Author);
+    if (title.value != "" && author.value != "" && pages.value != "") {
+        e.preventDefault();
+        const book = document.createElement("div");
+        book.classList.add("book");
 
-    const Pages = document.createElement("p");
-    Pages.classList.add("bookPages");
-    Pages.classList.add("text");
-    Pages.textContent = pages.value;
-    book.appendChild(Pages);
+        const Title = document.createElement("p");
+        Title.classList.add("bookTitle");
+        Title.classList.add("text");
+        Title.textContent = title.value;
+        title.value = "";
+        book.appendChild(Title);
 
+        const Author = document.createElement("p");
+        Author.classList.add("bookAuthor");
+        Author.classList.add("text");
+        Author.textContent = author.value;
+        author.value = "";
+        book.appendChild(Author);
 
-    const label = document.createElement("label");
-    label.classList.add("readButton");
-
-    const input = document.createElement("input");
-    input.type = "checkbox";
-    label.appendChild(input);
-
-    const span = document.createElement("span");
-    span.classList.add("buttonText");
-    span.classList.add("OnOff");
-    span.textContent = "Read";
-    label.appendChild(span);
-
-    book.appendChild(label);
+        const Pages = document.createElement("p");
+        Pages.classList.add("bookPages");
+        Pages.classList.add("text");
+        Pages.textContent = pages.value;
+        if (pages != "") {
+            totalPages.textContent = totalPages.textContent.slice(0, 13) + (Number(totalPages.textContent.slice(13)) + Number(pages.value));
+        }
+        pages.value = "";
+        book.appendChild(Pages);
 
 
-    const button = document.createElement("button");
-    button.classList.add("buttonText");
-    button.classList.add("removeButton");
-    button.textContent = "Remove";
-    book.appendChild(button);
+        const label = document.createElement("label");
+        label.classList.add("readButton");
 
-    button.addEventListener("click", () => {
-        button.parentNode.remove();
-    });
 
-    
-    library.appendChild(book);
+        const input = document.createElement("input");
+        input.type = "checkbox";
+        input.addEventListener("click", () => {
+            if (input.checked == "1") {
+                readBooks.textContent = readBooks.textContent.slice(0, -1) + (Number(readBooks.textContent.slice(-1)) + 1);
+            }
+            else {
+                readBooks.textContent = readBooks.textContent.slice(0, -1) + (Number(readBooks.textContent.slice(-1)) - 1);
+            }
+        });
+        if (bool) {
+            input.checked = true;
+            readBooks.textContent = readBooks.textContent.slice(0, -1) + (Number(readBooks.textContent.slice(-1)) + 1);
+        }
+        label.appendChild(input);
+
+
+
+        const span = document.createElement("span");
+        span.classList.add("buttonText");
+        span.classList.add("OnOff");
+        span.textContent = "Read";
+        label.appendChild(span);
+
+
+        book.appendChild(label);
+
+
+        const button = document.createElement("button");
+        button.classList.add("buttonText");
+        button.classList.add("removeButton");
+        button.textContent = "Remove";
+        book.appendChild(button);
+
+        button.addEventListener("click", () => {
+            button.parentNode.remove();
+            allBooks.textContent = allBooks.textContent.slice(0, -1) + library.children.length;
+            totalPages.textContent = totalPages.textContent.slice(0, 13) + (Number(totalPages.textContent.slice(13)) - Number(button.previousElementSibling.previousElementSibling.textContent));
+            if (button.previousElementSibling.firstElementChild.checked) {
+                readBooks.textContent = readBooks.textContent.slice(0, -1) + (Number(readBooks.textContent.slice(-1)) - 1);
+            }
+        });
+
+
+        library.appendChild(book);
+
+        allBooks.textContent = allBooks.textContent.slice(0, -1) + library.children.length;
+
+        popUp.style.display = "none";
+    }
 
 });
